6079eb03bc9b32203cd9d7c95c2f166a0cf50fa2
Luiz Otavio V. B. Oliveira <luiz.vilas.boas.oliveira@ing.com>
2025-03-27 10:49:56 +0100

    Deploy flow

--- main.py
+++ main.py
@@ -69,8 +69,9 @@ def run_kstars_flow(languages: dict[str, str]):
 
 
 if __name__ == "__main__":
-    run_kstars_flow.deploy(
-        name="run-kstars",
+    _ = run_kstars_flow.deploy(
+        name="kstars-load-api",
         parameters={"languages": LANGUAGES},
         work_pool_name="local",
+        cron="0 1 * * 5",
     )
