d9152817c5625be3157243a8860bb064b9d88b9c
Luiz Otavio Vilas Boas Oliveira <luiz.vbo@gmail.com>
2025-03-28 14:25:54 +0100

    Run 2 flows instead of 1

--- kstars/prefect-flow.py
+++ kstars/prefect-flow.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from typing import TypeVar
 
 import pandas as pd
-from prefect import flow, task
+from prefect import State, flow, serve, task
 from prefect.cache_policies import DEFAULT
 from prefect.concurrency.sync import rate_limit
 from prefect.logging import get_run_logger
@@ -52,7 +52,7 @@ OUTPUT_FOLDER = "../data"
 CACHE_POLICY = DEFAULT
 
 
-def human_readable_size(size_kb):
+def human_readable_size(size_kb: int) -> str:
     """Converts file size in KB to a human-readable format."""
     if size_kb < 1024:
         return f"{size_kb:.2f} KB"
@@ -69,33 +69,46 @@ def human_readable_size(size_kb):
 
 @task(
     tags=["kstars-data-processing"],
-    retries=10,
-    retry_delay_seconds=30,
-    cache_policy=CACHE_POLICY,
-    cache_expiration=timedelta(days=1),
+    retries=5,
+    retry_delay_seconds=10,
 )
-def preprocess_data(lang_name: str, input_folder: Path, output_folder: Path):
+def preprocess_data(
+    lang_name: str, input_folder: Path, output_folder: Path
+) -> None | State:
     logger = get_run_logger()
     fname = f"{lang_name}.csv"
-    path_input = Path(input_folder) / fname
+    input_file_path = Path(input_folder) / fname
+    output_file_path = Path(output_folder) / fname
+    output_top10_file_path = Path(output_folder) / f"top10_{fname}"
+
     try:
-        df: pd.DataFrame = pd.read_csv(path_input)
+        df: pd.DataFrame = pd.read_csv(input_file_path)
+        for col in ("Last Commit", "Created At"):
+            df[col] = df[col].apply(pd.to_datetime).dt.strftime("%d/%m/%Y")
+        df["Size"] = df["Size (KB)"].apply(human_readable_size)
+        new_columns = df.columns.drop("Size").tolist()
+        new_columns.insert(new_columns.index("Size (KB)"), "Size")
+        df = df[new_columns]
+        df.to_csv(output_file_path, index=False)
+        df.head(10).to_csv(output_top10_file_path, index=False)
+        logger.info(
+            f"Stored processed files to '{output_file_path}' and '{output_top10_file_path}'"
+        )
+
     except FileNotFoundError:
-        return Failed(message=f"File '{path_input}' not found.")
-
-    for col in ("Last Commit", "Created At"):
-        df[col] = df[col].apply(pd.to_datetime).dt.strftime("%d/%m/%Y")
-    df["Size"] = df["Size (KB)"].apply(human_readable_size)
-
-    new_columns = df.columns.drop("Size").tolist()
-    new_columns.insert(new_columns.index("Size (KB)"), "Size")
-    df = df[new_columns]
-
-    fname_out = Path(output_folder) / fname
-    fname_out_top10 = Path(output_folder) / f"top10_{fname}"
-    df.to_csv(fname_out, index=False)
-    df.head(10).to_csv(fname_out_top10, index=False)
-    logger.info(f"Stored processed files to '{fname_out}' and '{fname_out_top10}'")
+        return Failed(message=f"Error: Input file not found at '{input_file_path}'")
+    except KeyError as e:
+        return Failed(
+            message=f"Error: Column not found in CSV '{input_file_path}': {e}"
+        )
+    except ValueError as e:
+        return Failed(message=f"Error processing data in '{input_file_path}': {e}")
+    except PermissionError:
+        return Failed(
+            message=f"Error: Permission denied while writing to output folder '{output_folder}'"
+        )
+    except Exception as e:
+        return Failed(message=f"An unexpected error occurred: {e}")
 
 
 @task(
@@ -105,7 +118,7 @@ def preprocess_data(lang_name: str, input_folder: Path, output_folder: Path):
     cache_policy=CACHE_POLICY,
     cache_expiration=timedelta(days=1),
 )
-def run_kstars(language: str, lang_name: str, output_folder: str | Path):
+def run_kstars_task(language: str, lang_name: str, output_folder: str | Path) -> None|State:
     logger = get_run_logger()
     command = f"kstars -t $(cat access_token.txt) -l {language}:{lang_name} -o {output_folder}"
     print(f"Running command: {command}")
@@ -125,17 +138,16 @@ def run_kstars(language: str, lang_name: str, output_folder: str | Path):
     except Exception as e:
         return Failed(message=f"An unexpected error occurred: {e}")
 
-    return lang_name
 
 
 @flow(log_prints=True)
-def run_kstars_flow(languages: dict[str, str], output_folder: str):
+def run_kstars(languages: dict[str, str], output_folder: str):
     path_data_original = Path(output_folder) / "original"
     path_data_original.mkdir(parents=True, exist_ok=True)
 
     for lang, lang_name in languages.items():
         rate_limit("rate-limited-gh-api")
-        _ = run_kstars(lang, lang_name, path_data_original)
+        _ = run_kstars_task(lang, lang_name, path_data_original)
 
 
 @flow(log_prints=True)
@@ -144,12 +156,18 @@ def run_post_processing(languages: dict[str, str], output_folder: str):
     path_data_processed = Path(output_folder) / "processed"
     path_data_processed.mkdir(parents=True, exist_ok=True)
     for _, lang_name in languages.items():
-        preprocess_data(lang_name, path_data_original, path_data_processed)
+        _ = preprocess_data(lang_name, path_data_original, path_data_processed)
 
 
 if __name__ == "__main__":
-    _ = run_kstars_flow.serve(
+    flow_run_kstars = run_kstars.to_deployment(
         name="kstars-load-api",
         parameters={"languages": LANGUAGES, "output_folder": OUTPUT_FOLDER},
         cron="0 1 * * 5",
     )
+    flow_run_post_processing = run_post_processing.to_deployment(
+        name="kstars-data-processing",
+        parameters={"languages": LANGUAGES, "output_folder": OUTPUT_FOLDER},
+        cron="0 3 * * 5",
+    )
+    serve(flow_run_kstars, flow_run_post_processing)
